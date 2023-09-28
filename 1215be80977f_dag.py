191ef982d7c68edfafd84bd48e95e67a3e140b5c
Sai Charan Chandu <133879239+Charanaicore@users.noreply.github.com>
2023-09-28 06:07:55 +0100

    Add files via upload

---
+++ 1215be80977f_dag.py
@@ -0,0 +1,45 @@
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator, DatabricksRunNowOperator
+from datetime import datetime, timedelta 
+
+
+#Define params for Submit Run Operator
+notebook_task = {
+    'notebook_path': '/Users/paulio1977@hotmail.com/Batch Processing S3 Data',
+}
+
+
+#Define params for Run Now Operator
+notebook_params = {
+    "Variable":5
+}
+
+
+default_args = {
+    'owner': 'Paul Stevens',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=2)
+}
+
+
+with DAG('1215be80977f_dag',
+    # should be a datetime format
+    start_date=datetime(2023, 8, 20),
+    # check out possible intervals, should be a string
+    schedule_interval='@daily',
+    catchup=False,
+    default_args=default_args
+    ) as dag:
+
+
+    opr_submit_run = DatabricksSubmitRunOperator(
+        task_id='submit_run',
+        # the connection we set-up previously
+        databricks_conn_id='databricks_default',
+        existing_cluster_id='1108-162752-8okw8dgg',
+        notebook_task=notebook_task
+    )
+    opr_submit_run
